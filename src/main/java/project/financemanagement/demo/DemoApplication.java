991a6102cda157953ea0a80e2634cc7af5acfea2
Sezer Mehmed <sezer20001@gmail.com>
2023-05-03 01:09:24 +0300

    Initial commit

---
+++ src/main/java/project/financemanagement/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package project.financemanagement.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
